092e4147951248b9f78a5a13a53a92d2d5d228cb
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2022-10-31 10:51:46 +0600

    Video 16 completed

---
+++ 01-Fundamentals-Part-1/final/main.js
@@ -0,0 +1,19 @@
+var varvar = "varvar";
+let letvar = "letvar";
+
+function func()
+{
+    varvar2 = "varvar2";
+    letvar2 = "letvar2";
+
+    console.log("varvar is " + varvar);
+    console.log("letvar is " + letvar);
+
+    console.log("varvar2 is " + varvar2);
+    console.log("letvar2 is " + letvar2);
+}
+
+//console.log("varvar2 is " + varvar2);
+//console.log("letvar2 is " + letvar2);
+
+func();
