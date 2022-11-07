39cc30f4ada973320773307ca09474217c97455b
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2022-11-07 11:06:17 +0600

    Video 75 completed

--- 05-Guess-My-Number/GuessNumber/main.js
+++ 05-Guess-My-Number/GuessNumber/main.js
@@ -6,22 +6,23 @@ let againBtn = document.querySelector('.again');
 let inputNumber = 0;
 let score = Number(document.querySelector('.score').textContent);
 let checkBtnPressCount = 0;
-let agianBtnFunc = () => {
-    secretNumber = Math.trunc(Math.random() * 20) + 1;
-    document.querySelector('.message').textContent = `Start guessing...`;
-    document.querySelector('.guess').value = '';
-}
 
 console.log(`secret number is ${secretNumber}`);
 
 checkBtn.addEventListener(
     'click', 
     () => {
+
         ++checkBtnPressCount;
         console.log(checkBtnPressCount);
+
         if(checkBtnPressCount >= 10) {
-            document.querySelector('.message').textContent = `You lose!`;
-            agianBtnFunc();
+            document.querySelector('.message').textContent = `You lose!`;    
+            document.querySelector('.guess').value = '';
+            document.querySelector('.number').textContent = '?';
+            document.querySelector('body').style.backgroundColor = '#C10505';
+            document.querySelector('.check').disabled = true;
+            return;
         }
 
         inputNumber = Number(document.querySelector('.guess').value);
@@ -31,6 +32,9 @@ checkBtn.addEventListener(
         }
         else if(inputNumber == secretNumber) {
             document.querySelector('.message').textContent = `CORRECT!!! 😁`;
+            document.querySelector('.number').textContent = secretNumber;
+            document.querySelector('body').style.backgroundColor = '#17B169';
+            document.querySelector('.check').disabled = true;
         }
         else if(inputNumber > secretNumber) {
             document.querySelector('.message').textContent = `Try smaller numbers 🤔`;
@@ -45,4 +49,16 @@ checkBtn.addEventListener(
     }
 );
 
-againBtn.addEventListener('click', agianBtnFunc);
+againBtn.addEventListener (
+    'click', 
+    () => {
+        secretNumber = Math.trunc(Math.random() * 20) + 1;
+        checkBtnPressCount = 0;
+        document.querySelector('.message').textContent = `Start guessing...`;
+        document.querySelector('.guess').value = '';
+        document.querySelector('.number').textContent = '?';
+        document.querySelector('body').style.backgroundColor = '#222';
+        document.querySelector('.check').disabled = false;
+        console.log(`secret number is ${secretNumber}`);
+    }
+);
