5dc5e9563279fea3ab067eb9b4223afa55e4de7d
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2022-11-30 13:47:20 +0600

    Video 208 completed

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const User = function(userName, password) {
+    this.userName = userName;
+    this.password = password;
+
+    //Bad practice
+    this.showDetail = function() {
+        console.log(`Username: ${this.userName}\nPassword: ${this.password}`);
+    }
+}
+
+let userOne = new User('oop', 'oop#1');
+console.log(userOne);
+userOne.showDetail();
