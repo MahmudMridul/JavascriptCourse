c26454349fe544a161a3b3cad05f460891ef8959
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2022-12-05 11:11:51 +0600

    Video 209 completed, Jump to Section 16

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -13,3 +13,26 @@ const User = function(userName, password) {
 let userOne = new User('oop', 'oop#1');
 console.log(userOne);
 userOne.showDetail();
+userOne.address = 'address koi theke ashlo';
+console.log(userOne);
+
+const Client = function() {
+
+}
+
+let first = new Client();
+first.name = 'Abbas';
+first.address = 'ekta jayga'
+
+Client.prototype.getName = function() {
+    return this.name;
+}
+
+Client.prototype.getAddress = function() {
+    return this.address;
+}
+
+console.log(Client.prototype);
+
+console.log(first.getName());
+console.log(first.__proto__);
